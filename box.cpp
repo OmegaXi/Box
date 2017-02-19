fd014709bd714fc83a4de10e516ab1848afeb0b5
Yestin <641010761@qq.com>
2017-02-19 19:09:49 +0800

    0
    practice

---
+++ box.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+class Box
+{
+	public:
+		double length;
+		double breadth;
+		double height;
+}
+
+main()
+{
+	Box box1;
+	Box Box2;
+	double vol=0.0;
+	
+}
