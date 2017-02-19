24fd21ebdac258355e66f6f4315468b0554f7f21
Yestin <641010761@qq.com>
2017-02-19 19:13:24 +0800

    1
    补全

--- box.cpp
+++ box.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-class Box
+class box
 {
 	public:
 		double length;
@@ -12,8 +12,17 @@ class Box
 
 main()
 {
-	Box box1;
-	Box Box2;
+	box box1;
+	box Box2;
 	double vol=0.0;
-	
+	box1.height=5.0;
+	box1.length=6.0;
+	box1.breadth=7.0;
+	box2.height=10.0;
+	box2.length=12.0;
+	box2.breadth=13.0;
+	vol=box1.height*box1.length*box1.breadth;
+	cout<<"Box1's volume:"<<vol<<endl;
+	vol=box2.height*box2.length*box2.breadth;
+	cout<<"Box2's volume:"<<vol<<endl;
 }
