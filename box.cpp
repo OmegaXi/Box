8c885f0db51f724fcd7f6afa4f21e1ba4e0cf34b
Yestin <641010761@qq.com>
2017-02-19 19:21:30 +0800

    11
    change

--- box.cpp
+++ box.cpp
@@ -13,16 +13,13 @@ class box
 main()
 {
 	box box1;
-	box box2;
 	double vol=0.0;
-	box1.height=5.0;
-	box1.length=6.0;
-	box1.breadth=7.0;
-	box2.height=10.0;
-	box2.length=12.0;
-	box2.breadth=13.0;
+	cout<<"Input the height:"<<endl;
+	cin>>box1.height;
+	cout<<"Input the length:"<<endl;
+	cin>>box1.length;
+	cout<<"Input the breadth:"<<endl;
+	cin>>box1.breadth;
 	vol=box1.height*box1.length*box1.breadth;
 	cout<<"Box1's volume:"<<vol<<endl;
-	vol=box2.height*box2.length*box2.breadth;
-	cout<<"Box2's volume:"<<vol<<endl;
 }
